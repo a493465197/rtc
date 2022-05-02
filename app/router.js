bf50561d0fbbfcba3c02a50ba1c54a5ee45c230c
李哲宇 <lzhey@servyou.com.cn>
2022-05-02 16:15:20 +0800

    fix

--- app/router.js
+++ app/router.js
@@ -16,28 +16,14 @@ module.exports = app => {
   router.get('/api/isLogin', controller.api.isLogin);
   router.get('/api/getInfo', controller.api.getInfo);
   router.post('/api/setInfo', controller.api.setInfo);
-  router.post('/api/key', controller.api.key);
-  router.post('/api/keyList', controller.api.keyList);
-  // router.post('/api/yudin', controller.api.yudin);
-  // router.post('/api/yudinList', controller.api.yudinList);
-  // router.post('/api/setUser', controller.api.setUser);
   router.post('/api/userList', controller.api.userList);
   router.post('/api/delUser', controller.api.delUser);
-  // router.post('/api/gonggao', controller.api.gonggao);
-  // router.post('/api/gonggaoList', controller.api.gonggaoList);
 
-  router.post('/api/addKey', controller.api.addKey);
-  router.post('/api/myKeyList', controller.api.myKeyList);
-  router.post('/api/keyList', controller.api.keyList);
-  router.post('/api/delKey', controller.api.delKey);
   router.post('/api/docList', controller.api.docList);
   router.post('/api/logList', controller.api.logList);
   router.post('/api/docBack', controller.api.docBack);
   router.post('/api/delDoc', controller.api.delDoc);
   router.post('/api/addDoc', controller.api.addDoc);
-  router.post('/api/getKeyAuth', controller.api.getKeyAuth);
-  router.post('/api/getKeyAuthList', controller.api.getKeyAuthList);
-  router.post('/api/agreeGetKeyAuth', controller.api.agreeGetKeyAuth);
 
 
   router.post('/common/upload', controller.common.upload);
